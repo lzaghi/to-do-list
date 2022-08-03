99803ae3333f6d75dbba6f3e985e4a3acb15ca50
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-03 12:31:45 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -3,13 +3,32 @@ const botaoAdicionar = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const listaOl = document.getElementById('lista-tarefas');
 
+
+
 function adicionaTarefa() {
   const item = document.createElement('li');
   item.innerHTML = input.value;
-
+  item.className = 'item'
   listaOl.appendChild(item);
 
   input.value = '';
+
+  listaOl.appendChild(item);
+
+
+
+  function mudaFundo(event) {
+    const itensLi = document.getElementsByClassName('item')
+    
+    for (let i = 0; i < itensLi.length; i+= 1){
+      if (itensLi[i].style.backgroundColor == 'gray') {
+        itensLi[i].style.backgroundColor = 'white'
+      }
+    }
+    console.log(itensLi);
+    event.target.style.backgroundColor = 'gray'
+  }
+  item.addEventListener('click', mudaFundo)
 }
 
 botaoAdicionar.addEventListener('click', adicionaTarefa);
