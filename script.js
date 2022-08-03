867a898a10261da8cdf483670123c64b276655f4
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-03 15:22:35 -0300

    requisito 14

--- script.js
+++ script.js
@@ -20,9 +20,11 @@ function adicionaTarefa() {
     for (let i = 0; i < itensLi.length; i+= 1){
       if (itensLi[i].style.backgroundColor == 'gray') {
         itensLi[i].style.backgroundColor = 'white'
+        itensLi[i].classList.remove('selected')
       }
     }
     event.target.style.backgroundColor = 'gray'
+    event.target.classList.add('selected')
   }
   item.addEventListener('click', mudaFundo)
 
@@ -64,6 +66,54 @@ function adicionaTarefa() {
     }
   }
   botaoCompletos.addEventListener('click', removeCompletos)
+
+
+  // const botaoSalvar = document.getElementById('salvar-tarefas');
+
+  // function itensLocal() {
+  //   const listaAntiga = JSON.parse(localStorage.getItem('itens'));
+  //   const itensLi = document.getElementsByClassName('item');
+  //   console.log(itensLi);
+  //   if (itensLi.length > 0) {
+  //     for (let i = 0; i < itensLi.length; i += 1) {
+  //       listaAntiga.push(itensLi[i])
+  //     }
+      
+  //     localStorage.setItem('itens', JSON.stringify(listaAntiga))
+  //   }
+  // }
+  
+  // botaoSalvar.addEventListener('click', itensLocal);
+
+  const botaoRemover = document.getElementById('remover-selecionado')
+
+  function removeTarefa() {
+    const selecionado = document.querySelector('.selected')
+
+    selecionado.remove()
+  }
+  botaoRemover.addEventListener('click', removeTarefa)
 }
 
 botaoAdicionar.addEventListener('click', adicionaTarefa);
+
+
+// function initialRenderization() {
+//   if (localStorage.getItem('itens') === null) {
+//     localStorage.setItem('itens', JSON.stringify([]));
+//   } else {
+//     const itensLista = JSON.parse(localStorage.getItem('itens'));
+  
+//     for (let index = 0; index < itensLista.length; index += 1) {
+//       const listElement = document.createElement('li');
+
+//       listElement.innerText = phrasesList[index];
+
+//       list.appendChild(listElement);
+//     }
+//   }
+// }
+
+// window.onload = function() {
+//   initialRenderization();
+// };
