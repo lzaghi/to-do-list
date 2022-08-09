2073e68b8c651c385b4dc696249cfd2c8d9d3152
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-09 18:54:26 -0300

    requisito 13 vamoooos

--- script.js
+++ script.js
@@ -53,6 +53,10 @@ function removeTarefa() {
 }
 
 function jogaProLocal() {
+  // if (localStorage.itens != '[]') {
+  //   localStorage.clear()
+  // }
+
   let itens = document.getElementsByClassName('item');
   let array = JSON.parse(localStorage.getItem('itens'))
 
@@ -60,6 +64,72 @@ function jogaProLocal() {
     array.push(itens[i].outerHTML)
   }
   localStorage.setItem('itens', JSON.stringify(array))
+}
+
+function moveUp() {
+  let selected = document.querySelector('.selected')
+  // console.log(selected);
+  // console.log(selected == listaOl.children[0]);
+
+  let array = []
+  for (let ji = 0; ji < listaOl.children.length; ji += 1){
+    array.push(listaOl.children[ji])
+  }
+  // console.log(array);
+  // console.log(selected == array[0]);
+
+  if (selected != listaOl.children[0]) {
+    let indexSel;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] == selected) {
+        indexSel = i
+      }
+    }
+    // console.log(indexSel);
+    // console.log(listaOl.children[indexSel]);
+    // console.log(listaOl.children[indexSel-1]);
+  
+    let temporary = array[indexSel]
+    // console.log(temporary);
+    array[indexSel] = array[indexSel-1]
+    // console.log(array[indexSel]);
+    array[indexSel-1] = temporary;
+    // console.log(array[indexSel-1]);
+
+    // console.log(array);
+
+    // console.log(listaOl.children)
+    listaOl.innerHTML = ''
+
+    for (let k = 0; k < array.length; k += 1){
+      listaOl.appendChild(array[k])
+    }
+  //   let temporaryClass = listaOl.children[indexSel].innerHTML
+  //   console.log(temporaryClass);
+  }
+}
+
+function moveDown() {
+  let selected = document.querySelector('.selected')
+  let array = []
+  for (let ji = 0; ji < listaOl.children.length; ji += 1){
+    array.push(listaOl.children[ji])
+  }
+  if (selected != listaOl.children[listaOl.children.length-1]) {
+    let indexSel;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] == selected) {
+        indexSel = i
+      }
+    }
+    let temporary = array[indexSel]
+    array[indexSel] = array[indexSel+1]
+    array[indexSel+1] = temporary;
+    listaOl.innerHTML = ''
+    for (let k = 0; k < array.length; k += 1){
+      listaOl.appendChild(array[k])
+    }
+  }
 }
 
 function adicionaTarefa() {
@@ -71,18 +141,28 @@ function adicionaTarefa() {
   input.value = '';
 
   listaOl.appendChild(item);
-
-  
-  item.addEventListener('click', mudaFundo)
-
-  item.addEventListener('dblclick', riscaItem);
+  // console.log(localStorage.itens);
+  // console.log(listaOl.children);
+  let todos = listaOl.children
+  for (let i = 0; i < todos.length; i += 1){
+    todos[i].addEventListener('click', mudaFundo)
+    todos[i].addEventListener('dblclick', riscaItem);
+  }
+  // item.addEventListener('click', mudaFundo)
+  // item.addEventListener('dblclick', riscaItem);
 
   const botaoLimpar = document.getElementById('apaga-tudo');
   botaoLimpar.addEventListener('click', limpaLista);
 
+  const botaoUp = document.getElementById('mover-cima');
+  botaoUp.addEventListener('click', moveUp)
+
+  const botaoDown = document.getElementById('mover-baixo');
+  botaoDown.addEventListener('click', moveDown)
+
   const botaoCompletos = document.getElementById('remover-finalizados');
   botaoCompletos.addEventListener('click', removeCompletos)
-  
+
   const botaoSalvar = document.getElementById('salvar-tarefas'); 
   botaoSalvar.addEventListener('click', jogaProLocal);
 
@@ -91,6 +171,14 @@ function adicionaTarefa() {
 }
 
 botaoAdicionar.addEventListener('click', adicionaTarefa);
+
+let todos = listaOl.children
+for (let i = 0; i < todos.length; i += 1){
+  todos[i].addEventListener('click', mudaFundo)
+
+  todos[i].addEventListener('dblclick', riscaItem);
+}
+
 
 function pegaDoLocal() {
   if (localStorage.getItem('itens') === null) {
